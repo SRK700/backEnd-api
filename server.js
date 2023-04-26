5ea5b2f1f1cbf2fdf99cfc9b4f57b74a7307270c
SRK700 <papana.210@gmail.com>
2023-04-26 13:16:14 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+require('express-async-errors')
+const express = require('express')
+const app = express()
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./src/config/corsOptons')
+const mongoose = require('mongoose')
+const connectDB = require('./src/config/dbConn')
+
+connectDB()
+
+app.use(express.json())
+app.use(cors(corsOptions))
+app.use(cookieParser())
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+/* MONGOOSE SETUP */
+const port = process.env.PORT || 5001
+
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB')
+  app.listen(port, () => console.log(`Server is runing with port ${port}`))
+})
+
+mongoose.connection.on('error', (err) => console.log(err))
